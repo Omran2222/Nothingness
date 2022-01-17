43267f06fa67b15a937e745b65f43a03d2b5fcf0
omran <omran@DESKTOP-P3GRMH5>
2022-01-17 19:59:58 +0300

    Add project files.

---
+++ Nothingness/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nothingness.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
